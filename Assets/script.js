a04aaf3bf7e01b341baec1848e3a936af4910224
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2021-11-12 17:31:43 -0500

    initial updates

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(){
+var value = $(this).siblings(".description").val()
+var time = $(this).parent().attr("id")
+localStorage.setItem(value,time)
+    })
+    function hourupdater (){
+        var currenthour = moment().hours()
+        $(".time-block").each(function(){
+            var hourblock = parseInt($(this).attr("id").split("-")[1])
+        })
+    }
+})
+
+//add if else statement to compare the current hour with the hour block to add a class from css or remove class from css

ce0e3d3e71ca8a4adacee81b267dc71a3bda2e03
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2021-11-20 16:47:41 -0500

    updated JS

--- Assets/script.js
+++ Assets/script.js
@@ -2,28 +2,36 @@ $(document).ready(function(){
     $(".saveBtn").on("click", function(){
 var value = $(this).siblings(".description").val()
 var time = $(this).parent().attr("id")
-localStorage.setItem(value,time)
+localStorage.setItem(time,value)
     })
     function hourupdater (){
         var currenthour = moment().hours()
         $(".time-block").each(function(){
             var hourblock = parseInt($(this).attr("id").split("-")[1])
+        
+        if (hourblock < currenthour) {
+            $(this).addClass("past")
+        } else if (hourblock === currenthour) {
+            $(this).removeClass("past")
+            $(this).addClass("present")
+        }
+        
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future") 
+        
+        }
         })
     }
+    hourupdater()
+    var interval = setInterval(hourupdater, 30000)
+  // add line 30 for each hour
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))   
+    $("#currentDay").text(moment().format("MMM Do YY"))
+    
 })
 
-//add if else statement to compare the current hour with the hour block to add a class from css or remove class from css
-function time() {
-    if (currenthour.textContent = time) {
-        setAttribute("present")
-     }
-     if (currenthour.textContent <= time) {
-        setAttribute("future")
-    }
-    
-    if (currenthour.textContent >= time) {
-        setAttribute("past")
-    }
-}
+
 
 
